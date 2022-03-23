1bc63ca602d9fb449efb5ec6e4103380057a342e
Clara Jegousse <clara.jegousse@gmail.com>
2022-03-23 17:59:47 +0000

    profiling

--- scripts/002-mapping.sh
+++ scripts/002-mapping.sh
@@ -1,17 +1,6 @@
-#!/usr/bin/env bash
-#SBATCH --job-name=download-marine-mg
-#SBATCH -p normal
-#SBATCH --time=2-00:00:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=cat3@hi.is
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-
-echo $HOSTNAME
-
 # to insure work with python3
 source /users/home/cat3/.bashrc
-conda activate anvio-master
+#conda activate anvio-master
 
 WD=/users/home/cat3/projects/mime-eco
 CONTIGS_DIR=$WD/sunbeam_output/assembly/contigs
@@ -23,50 +12,46 @@ NUM_THREADS=12
 cd $CONTIGS_DIR
 FILES=$CONTIGS_DIR/*.fa
 
-#for file in $FILES
-#do
-#	sample=$(basename "$file" | cut -f 1 -d "-")
-#	bowtie2-build $file $MAPDIR/$sample
-#done
-
-
 for file in $FILES
 do
 	sample=$(basename "$file" | cut -f 1 -d ".")
 echo $sample
-echo '''#!/bin/bash
-#SBATCH --job-name='$sample'-bowtie2
+echo '''#!/usr/bin/env bash
+#SBATCH --job-name=profiling
 #SBATCH -p normal
-#SBATCH --time=10:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=cat3@hi.is
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
-#SBATCH --output='$sample'-bowtie2.%j.out
-#SBATCH --error='$sample'-bowtie2.%j.err
 
 echo $HOSTNAME
-source /users/home/cat3/.bashrc
-conda activate anvio-master
 
+# to insure work with python3
+source /users/home/cat3/.bashrc
 
 WD=/users/home/cat3/projects/mime-eco
 CONTIGS_DIR=$WD/sunbeam_output/assembly/contigs
 RAW_DATA_DIR=$WD/data
 MAPDIR=$WD/sunbeam_output/mapping
+PROFILES_DIR=$WD/sunbeam_output/profiles
 NUM_THREADS=12
 
-# go to working directory
-cd $CONTIGS_DIR
-FILES=$CONTIGS_DIR/*-contigs.fa
+sample='$sample'
 
+conda activate anvio-master
 
-# echo $file $new_name
-bowtie2 --threads $NUM_THREADS -x $MAPDIR/'$sample' -1 $RAW_DATA_DIR/'$sample'_1.fastq.gz -2 $RAW_DATA_DIR/'$sample'_2.fastq.gz -S $MAPDIR/'$sample'.sam
-# samtools view -F 4 -bS $MAPDIR/'$sample'.sam > $MAPDIR/'$sample'-RAW.sam
-# anvi-init-bam $MAPDIR/'$sample'-RAW.sam -o $MAPDIR/'$sample'.bam
-# rm 04_MAPPING/Sample_01.sam 04_MAPPING/Sample_01-RAW.bam
+bowtie2-build $CONTIGS_DIR/$sample".fa" $MAPDIR/$sample
+bowtie2 --threads $NUM_THREADS -x $MAPDIR/$sample -1 $RAW_DATA_DIR/$sample"_1.fastq.gz" -2 $RAW_DATA_DIR/$sample"_2.fastq.gz" -S $MAPDIR/$sample".sam"
 
-''' > $WD/$sample-bowtie-pbs.sh
+conda deactivate
+
+samtools view -F 4 -bS $MAPDIR/$sample".sam" > $MAPDIR/$sample"-RAW.sam"
+
+conda activate anvio-master
+
+anvi-init-bam $MAPDIR/$sample"-RAW.sam" -o $MAPDIR/$sample".bam"
+anvi-profile -i $MAPDIR/$sample".bam" -c $CONTIGS_DIR/$sample".db" -o $PROFILES_DIR/$sample -T $NUM_THREADS
+''' > $WD/$sample-mapping-pbs.sh
 
 done
